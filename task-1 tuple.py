18e9b54b77b593803ed3d761b92573098a6135cb
Pallaviediga <161039925+Pallaviediga@users.noreply.github.com>
2025-06-25 18:11:27 +0530

    Add files via upload

--- task-1 tuple.py
+++ task-1 tuple.py
@@ -1,28 +1,28 @@
-1. Acess the value 200 from this nested tuple?
+# 1. Acess the value 200 from this nested tuple?
 g=(10,20,(30,40,(100,200)),50)
-print(g[2][2][0])
+print(g[2][2][1])
 
-output:
-200
+# output:
+# 200
 
-2.Accessvthe value 70 fromthe nested structure?
+# 2.Accessvthe value 70 fromthe nested structure?
 h=(10,(20,30,(40,50,(60,70))),80)
 print(h[1][2][2][1])
 
-output:
-70
+# output:
+# 70
 
-3.convert j=(10,20,30,40,50) tuple data type to list datatype?
+# 3.convert j=(10,20,30,40,50) tuple data type to list datatype?
 j=(10,20,30,40,50)
 res=list(j)
 print(res)
 print(type(res))
 
-output:
-[10,20,30,40,50]
-<class 'list'>
+# output:
+# [10,20,30,40,50]
+# <class 'list'>
 
-4.Find common elements between two tuples?
+# 4.Find common elements between two tuples?
 k=(1,2,3,4)
 h=(3,4,5,6)
 res=[]
@@ -31,5 +31,5 @@ for i in k:
         res.append(i)
 print(res)    
 
-output:
-[3,4]
+# output:
+# [3,4]
